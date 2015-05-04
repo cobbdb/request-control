26bdf6fd5350915c5c240e9518866b68bf29ff95
Dan <cobbdb@gmail.com>
2015-05-04 15:36:39 -0700

    report high reqs/sec

--- ajax-spy.js
+++ ajax-spy.js
@@ -1,11 +1,32 @@
+var reqCnt = {
+        net: 0
+    },
+    lowpass = 5;
+
 function AjaxSpy(id) {
     var frame = document.getElementById(id),
         oldajax = frame.contentWindow.XMLHttpRequest;
     frame.contentWindow.XMLHttpRequest = function (args) {
         console.log(
-            '> Spying on XMLHttpRequest object of: ',
+            '<AjaxSpy> Spying on XMLHttpRequest object of: ',
             id
         );
-        return new oldajax(args);
+        reqCnt.net += 1;
+        var req = new oldajax(args);
+        req.addEventListener('load', function (event) {
+            reqCnt[req.status] = reqCnt[req.status] || 0;
+            reqCnt[req.status] += 1;
+        }, false);
+        return req;
     };
 }
+
+setInterval(function () {
+    if (reqCnt.net > lowpass) {
+        console.log(
+            '<AjaxSpy> !!!! reqs/sec is too high !!!!',
+            reqCnt.net
+        );
+    }
+    reqCnt.net = 0;
+}, 1000);
