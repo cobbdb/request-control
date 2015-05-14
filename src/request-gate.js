2f3ab93fc88b30105c5bece2a87afbe9ebdc9079
Dan <cobbdb@gmail.com>
2015-05-14 15:44:57 -0700

    strict close on gate context

--- src/request-gate.js
+++ src/request-gate.js
@@ -4,16 +4,17 @@ var StatSet = require('./stat-set.js');
  * @param {String} name Unique identifier for this gate.
  * @param {Number} [opts.throttle] Defaults to 1000 milliseconds.
  * @param {String} opts.id Element id of parent frame.
- * @param {Window} opts.context
+ * @param {Window} [opts.context]
  * @return {Object}
  */
 module.exports = function (name, opts) {
-    opts.throttle = opts.throttle || 1000;
+    var throttle = opts.throttle || 1000,
+        context = opts.context || global.self;
 
-    opts.context.rcStats = opts.context.rcStats || {};
-    opts.context.rcStats[name] = opts.context.rcStats[name] || StatSet(name, opts.id);
-    opts.context.rcLast = opts.context.rcLast || {};
-    opts.context.rcLast[name] = opts.context.rcLast[name] || 0;
+    context.rcStats = context.rcStats || {};
+    context.rcStats[name] = context.rcStats[name] || StatSet(name, opts.id);
+    context.rcLast = context.rcLast || {};
+    context.rcLast[name] = context.rcLast[name] || 0;
 
     return {
         /**
@@ -21,22 +22,22 @@ module.exports = function (name, opts) {
          */
         check: function () {
             var now = global.Date.now(),
-                firstReq = !opts.context.rcLast[name],
-                greenLight = now - opts.context.rcLast[name] > opts.throttle;
-            opts.context.rcStats[name].count.attempted();
+                firstReq = !context.rcLast[name],
+                greenLight = now - context.rcLast[name] > throttle;
+            context.rcStats[name].count.attempted();
             if (firstReq || greenLight) {
                 this.close();
-                opts.context.rcStats[name].count.made();
+                context.rcStats[name].count.made();
                 return true;
             }
             return false;
         },
         close: function () {
-            opts.context.rcLast[name] = global.Date.now();
+            context.rcLast[name] = global.Date.now();
         },
         open: function () {
-            opts.context.rcLast[name] = 0;
+            context.rcLast[name] = 0;
         },
-        stats: opts.context.rcStats[name]
+        stats: context.rcStats[name]
     };
 };
